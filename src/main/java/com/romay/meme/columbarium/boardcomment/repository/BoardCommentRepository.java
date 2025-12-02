464e4c223ce7e7688a7b721137625fcc30fde457
LeeSooBin <romaydev@naver.com>
2025-12-02 15:05:50 +0900

    댓글 오래된 순으로 정렬 기준 변경

--- src/main/java/com/romay/meme/columbarium/boardcomment/repository/BoardCommentRepository.java
+++ src/main/java/com/romay/meme/columbarium/boardcomment/repository/BoardCommentRepository.java
@@ -15,7 +15,7 @@ public interface BoardCommentRepository extends JpaRepository<BoardComment, Long
 
   // Fetch Join 으로 멤버 닉네임까지 한번에 가져오자
   @Query("SELECT c from BoardComment c JOIN FETCH c.member "
-      + "WHERE c.boardCode = :board ORDER BY c.createdAt DESC")
+      + "WHERE c.boardCode = :board ORDER BY c.createdAt ASC")
   Page<BoardComment> findAllWithMemberByBoardCode(@Param("board") Long boardCode,
       Pageable pageable);
 
